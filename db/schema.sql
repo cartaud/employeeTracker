5281db394c3656e392a97c3c57cb716a13da7d1f
Chadd <chadd_artaud@cox.net>
2022-06-08 13:20:16 -0700

    created module to read and write to db

--- db/schema.sql
+++ db/schema.sql
@@ -45,3 +45,55 @@ VALUES ("BOB", "BUILDER", 1, 2),
        ("JACK", "INDABOX", 1, 2),
        ("ELON", "MUSK", 1, 2),
        ("JEFF", "BEZOS", 1, 2);
+       
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+);
+
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1),
+       ("Salesperson", 80000, 1),
+       ("Lead Engineer", 150000, 2),
+       ("Software Engineer", 120000, 2),
+       ("Account Manager", 160000, 3),
+       ("Accountant", 125000, 3),
+       ("Legal Team Lead", 250000, 4),
+       ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("BOB", "BUILDER", 1, 2),
+       ("JACK", "INDABOX", 1, 2),
+       ("ELON", "MUSK", 1, 2),
+       ("JEFF", "BEZOS", 1, 2);
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const inquirer = require('inquirer') ; //package that is used to prompt the user to answering questions 
+const inquirer = require('inquirer'); //package that is used to prompt the user to answering questions 
+const {readAndAppend} = require('./helpers/fsUtils');
 const mysql = require('mysql2');
 
 const db = mysql.createConnection(
@@ -21,35 +22,34 @@ const menu = () => {
             choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
         }
     ])
-    .then(actionSelector)
+    .then(menuSelection)
 }
 
-const actionSelector = (data) => {
+const menuSelection = (data) => {
     if (data.action == 'view all departments') {
-        view('department')
+        viewTable('department')
     }
     else if (data.action == 'view all roles') {
-        view('roles')
+        viewTable('roles')
     } 
     else if (data.action == 'view all employees') {
-        view('employee')
+        viewTable('employee')
     } 
     else if (data.action == 'add a department') {
-        getDepartmentData()
+        collectData('department')
     } 
     else if (data.action == 'add a role') {
-        getRoleData()
+        collectData('roles')
     } 
     else if (data.action == 'add an employee') {
-        getEmployeeData()
+        collectData('employee')
     } 
     else if (data.action == 'update an employee role') {
         console.log('update an employee role')
     } 
 }
 
-
-const view = (selection) => {
+const viewTable = (selection) => {
     let join
     if (selection == 'department') {
         join = ''
@@ -67,55 +67,51 @@ const view = (selection) => {
     })
 }
 
-const getDepartmentData = () => {
-    inquirer
-    .prompt([
+const collectData = (selection) => {
+    if (selection == 'department') {
+        inquirer
+        .prompt([
         {
             type: 'input',
             message: 'What is the name of the department?',
             name: 'departmentName',
         }
     ])
-    .then(createDepartment);
-}
-
-const createDepartment = (data) => {
-    //read and write to department db
-    console.log(`Added ${data.departmentName} to the database`)
-    menu()
-}
-
-const getRoleData = () => {
-    inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the name of the role?',
-            name: 'roleName',
-        },
-        {
-            type: 'input',
-            message: 'What is the salary of the role?',
-            name: 'roleSalary',
-        },
-        {
-            type: 'input',
-            message: 'Which department does the role belong to?',
-            name: 'roleDepartment',
-        },
-    ])
-    .then(createRole);
-}
-
-const createRole = (data) => {
-    //read and write to role db
-    console.log(`Added ${data.roleName} to the database`)
-    menu()
-}
-
-const getEmployeeData = () => {
-    inquirer
-    .prompt([
+    .then(addToDb);
+    }
+    else if (selection == 'roles') {
+        const departmentList = []
+        db.query(`SELECT name FROM department`, function (err, results) {
+            if (err) throw err
+            results.forEach((department) => {
+                departmentList.push(department.name)
+            })
+        })
+        
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the name of the role?',
+                name: 'roleName',
+            },
+            {
+                type: 'input',
+                message: 'What is the salary of the role?',
+                name: 'roleSalary',
+            },
+            {
+                type: 'list',
+                message: 'Which department does the role belong to?',
+                name: 'roleDepartment',
+                choices: departmentList
+            },
+        ])
+        .then(addToDb);
+    }
+    else if (selection == 'employee') {
+        inquirer
+        .prompt([
         {
             type: 'input',
             message: 'What is the employee\'s first name?',
@@ -137,12 +133,32 @@ const getEmployeeData = () => {
             name: 'employeeManager',
         }
     ])
-    .then(createEmployee);
+    .then(addToDb);
+    }
 }
 
-const createEmployee = (data) => {
-    //read and write to employee db
-    console.log(`Added ${data.employeeFirst} ${data.employeeLast} to the database`)
+const addToDb = (data) => {
+    const dbTable = Object.keys(data)[0]
+    let dbInput
+    if (/department/.test(dbTable)) {
+        dbInput = 
+        `INSERT INTO department (name) 
+        VALUES ("${data.departmentName}")`
+        console.log(`Added ${data.departmentName} to the database`)
+    }
+    else if (/role/.test(dbTable)) {
+        dbInput = 
+        `INSERT INTO roles (title, salary, department_id)
+        VALUES ("${data.roleName}", ${data.roleSalary}, "${data.roleDepartment}")`
+        console.log(`Added ${data.roleName} to the database`)
+    }
+    else if (/employee/.test(dbTable)) {
+        dbInput =
+        `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+        VALUES ("${data.employeeFirst}", "${data.employeeLast}", "${data.employeeRole}", "${data.employeeManager}")`
+        console.log(`Added ${data.employeeFirst} ${data.employeeLast} to the database`)
+    }
+    //readAndAppend(dbInput, './db/schema.sql')
     menu()
 }
 
