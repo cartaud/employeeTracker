17b334d3c6dc2360292cc532e98ba0d089e2d8be
Chadd <chadd_artaud@cox.net>
2022-06-10 10:11:13 -0700

    modularized all db methods

--- helpers/query.js
+++ helpers/query.js
@@ -58,6 +58,66 @@ class Query {
             else console.log(`\nAdded ${name} to the database`)
         })
     }
+
+    addRole(data) {
+        db.query(`SELECT id FROM department WHERE name = '${data.roleDepartment}'`, function(err, result) {
+            if (err) throw err
+            const roleEntry = new Query(`INSERT INTO roles (title, salary, department_id) VALUES ("${data.roleName}", ${data.roleSalary}, ${result[0].id});`)
+            roleEntry.addData(data.roleName) 
+        })
+    }
+
+    addEmployee(data) {
+        db.query(`SELECT id FROM roles WHERE title = '${data.employeeRole}'`, function(err, result) {
+            if (err) throw err
+            const roleId = result[0].id
+            if (data.employeeManager == 'None') {
+                const employeeEntry = new Query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${data.employeeFirst}", "${data.employeeLast}", ${roleId}, NULL);`)
+                employeeEntry.addData(data.employeeFirst + ' ' + data.employeeLast)
+            }  
+            else {
+                const nameArr = data.employeeManager.split(" ")
+                db.query(`SELECT id FROM employee WHERE first_name REGEXP '${nameArr[0]}' AND last_name REGEXP '${nameArr[1]}'`, function(err,result) {
+                    if (err) throw err
+                    const employeeEntry = new Query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${data.employeeFirst}", "${data.employeeLast}", ${roleId}, ${result[0].id});`)
+                    employeeEntry.addData(data.employeeFirst + ' ' + data.employeeLast)
+                })
+            }   
+        })
+    }
+
+    listDepartment() {
+        const departmentList = [];
+        db.query(`SELECT name FROM department`, function (err, results) {
+            if (err) throw err
+            results.forEach((department) => {
+                departmentList.push(department.name)
+            })
+        })
+        return departmentList
+    }
+
+    listRoles() {
+        const roleList = [];
+        db.query(`SELECT title FROM roles`, function (err, results) {
+            if (err) throw err
+            results.forEach((role) => {
+                roleList.push(role.title)
+            })
+        })
+        return roleList
+    }
+
+    listManager() {
+        const managerList = ['None'];
+        db.query(`SELECT first_name, last_name FROM employee WHERE manager_id is null`, function (err, results) {
+            if (err) throw err
+            results.forEach((manager) => {
+                managerList.push(manager.first_name + ' ' + manager.last_name)
+            })
+        })
+        return managerList
+    }
 }
 
 module.exports = Query
--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 //Questions: 70, 82, 128
 const inquirer = require('inquirer'); //package that is used to prompt the user to answering questions 
 const Query = require('./helpers/query');
-const mysql = require('mysql2');
-const cTable = require('console.table');
-
-
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'rootroot',
-        database: 'employee_db'
-    },
-    console.log('Connected to the employee_db database')
-)
 
 const menu = () => {
     inquirer
@@ -22,40 +9,43 @@ const menu = () => {
             type: 'list',
             message: 'What would you like to do?',
             name: 'action',
-            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Quit']
         }
     ])
     .then(menuSelection)
 }
 
 const menuSelection = (data) => {
-    if (data.action == 'view all departments') {
+    if (data.action == 'View All Departments') {
         const departmentTable = new Query(`SELECT * FROM department`)
         departmentTable.getDepartment();
         timeout();
     }
-    else if (data.action == 'view all roles') {
+    else if (data.action == 'View All Roles') {
         const rolesTable = new Query(`SELECT roles.id, roles.title, roles.salary, department.name AS department FROM roles JOIN department ON department.id = roles.department_id;`)
         rolesTable.getRole();
         timeout();
     } 
-    else if (data.action == 'view all employees') {
+    else if (data.action == 'View All Employees') {
         const employeeTable = new Query(`SELECT employee.id, employee.first_name, employee.last_name, roles.title AS title, department.name AS department, roles.salary AS salary, manager.first_name AS manager_first, manager.last_name AS manager_last FROM employee JOIN roles ON employee.role_id = roles.id  JOIN department ON department.id = roles.department_id LEFT JOIN employee AS manager ON employee.manager_id = manager.id;`)
         employeeTable.getEmployee();
         timeout();
     } 
-    else if (data.action == 'add a department') {
+    else if (data.action == 'Add Department') {
         collectData('department')
     } 
-    else if (data.action == 'add a role') {
+    else if (data.action == 'Add Role') {
         collectData('roles')
     } 
-    else if (data.action == 'add an employee') {
+    else if (data.action == 'Add Employee') {
         collectData('employee')
     } 
-    else if (data.action == 'update an employee role') {
+    else if (data.action == 'Update Employee Role') {
         console.log('update an employee role')
     } 
+    else if (data.action == 'Quit') {
+        process.exit(1)
+    }
 }
 
 const collectData = (selection) => {
@@ -71,14 +61,7 @@ const collectData = (selection) => {
     .then(addToDb);
     }
     else if (selection == 'roles') {
-        const departmentList = []
-        db.query(`SELECT name FROM department`, function (err, results) {
-            if (err) throw err
-            results.forEach((department) => {
-                departmentList.push(department.name)
-            })
-        })
-        
+        const queryDepartment = new Query() 
         inquirer
         .prompt([
             {
@@ -95,27 +78,15 @@ const collectData = (selection) => {
                 type: 'list',
                 message: 'Which department does the role belong to?',
                 name: 'roleDepartment',
-                choices: departmentList 
+                choices: queryDepartment.listDepartment() 
             },
         ])
         .then(addToDb);
     }
 
     else if (selection == 'employee') {
-        const roleList = [];
-        const managerList = ['None'];
-        db.query(`SELECT title FROM roles`, function (err, results) {
-            if (err) throw err
-            results.forEach((role) => {
-                roleList.push(role.title)
-            })
-        })
-        db.query(`SELECT first_name, last_name FROM employee WHERE manager_id is null`, function (err, results) {
-            if (err) throw err
-            results.forEach((manager) => {
-                managerList.push(manager.first_name + ' ' + manager.last_name)
-            })
-        })
+        const queryRoles = new Query() 
+        const queryManager = new Query() 
         inquirer
         .prompt([
         {
@@ -132,13 +103,13 @@ const collectData = (selection) => {
             type: 'list',
             message: 'What is the employee\'s role?',
             name: 'employeeRole',
-            choices: roleList
+            choices: queryRoles.listRoles()
         },
         {
             type: 'list',
             message: 'Who is the employee\'s manager?',
             name: 'employeeManager',
-            choices: managerList
+            choices: queryManager.listManager()
         }
     ])
     .then(addToDb);
@@ -149,34 +120,15 @@ const addToDb = (data) => {
     const dbTable = Object.keys(data)[0]
     if (/department/.test(dbTable)) {
         const departmentEntry = new Query( `INSERT INTO department (name) VALUES ("${data.departmentName}");`)
-        departmentEntry.addData(data.departmentName)
+        departmentEntry.addData(data.departmentName) //adds specified data to db
     }
     else if (/role/.test(dbTable)) {
-        db.query(`SELECT id FROM department WHERE name = '${data.roleDepartment}'`, function(err, result) {
-            if (err) throw err
-            const roleEntry = new Query(`INSERT INTO roles (title, salary, department_id) VALUES ("${data.roleName}", ${data.roleSalary}, ${result[0].id});`)
-            roleEntry.addData(data.roleName) 
-        })
+        const roleEntry = new Query() 
+        roleEntry.addRole(data) //this query first gets the id of the role the user chose, then calls addData
     }
     else if (/employee/.test(dbTable)) {
-        let managerId 
-        db.query(`SELECT id FROM roles WHERE title = '${data.employeeRole}'`, function(err, result) {
-            if (err) throw err
-            const roleId = result[0].id
-            if (data.employeeManager == 'None') {
-                managerId = null;
-            }  
-            else {
-                const nameArr = data.employeeManager.split(" ")
-                db.query(`SELECT id FROM employee WHERE first_name REGEXP '${nameArr[0]}' AND last_name REGEXP '${nameArr[1]}'`, function(err,result) {
-                    if (err) throw err
-                    managerId = result[0].id
-                })
-            }   
-            const employeeEntry = new Query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${data.employeeFirst}", "${data.employeeLast}", ${roleId}, ${managerId});`)
-            employeeEntry.addData(data.employeeFirst + ' ' + data.employeeLast)
-            
-        })
+        const employeeEntry = new Query() //this query first gets the id of role and manager then calls addData
+        employeeEntry.addEmployee(data)
     }
     timeout();
 }
@@ -188,7 +140,5 @@ const timeout = () => {
 menu()
 
 /*toDO: 
-when adding data, need to have corresponding id for users input (should be integer not string). Maybe need to REGEX to with input department and return id
-add exit method on menu
 Look at activity numbers 21 and 22 for DELETE method
 */
